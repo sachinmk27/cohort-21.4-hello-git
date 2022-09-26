b2122937886cdfd7299ca7eb66ca1d95d703a25c
prem <sonanios2010@gmail.com>
2022-09-26 22:23:10 +0530

    hello

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ console.log('Commiting after adding the SSH key by Vikram')
 console.log('Commiting after adding the SSH key by Vikram')
 
 console.log("updating");
+
+console.log("commiting changes by prem after creating ssh key");
+
+console.log("please accept changes made by prem dhan");
